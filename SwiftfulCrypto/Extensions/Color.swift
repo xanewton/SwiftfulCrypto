1bafa904c55a533c5709ce580e910e8b84039abb
Angel Newton <xanewton@gmail.com>
2022-06-23 20:47:31 -0400

    Setting up Xcode Project and a custom color theme

--- SwiftfulCrypto/Extensions/Color.swift
+++ SwiftfulCrypto/Extensions/Color.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+}
+
+// This is adaptable because: we can add more colors to the theme or new themes, we can use this colors in the app code.
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
